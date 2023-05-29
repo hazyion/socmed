7a34caa88534edecc26c4b4f4fba597c435cf649
Carl Jason <73461353+Carl-bot-Jason@users.noreply.github.com>
2023-05-29 13:26:44 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
       '/api': {
         target: 'https://socmed-server.vercel.app/api',
         changeOrigin: true,
-        secure: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
     }
